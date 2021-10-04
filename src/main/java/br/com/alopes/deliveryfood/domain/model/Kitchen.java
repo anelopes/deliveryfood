d392947a08b1e668edba126e47f206c28791ef0f
anelopes <anelopes17@outlook.com>
2021-10-04 09:02:23 -0300

    Adicionando Lombok

--- src/main/java/br/com/alopes/deliveryfood/domain/model/Kitchen.java
+++ src/main/java/br/com/alopes/deliveryfood/domain/model/Kitchen.java
@@ -1,46 +1,22 @@
 package br.com.alopes.deliveryfood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) //gera somente para os campos que especifica ser incluso
 @Entity
 public class Kitchen {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Kitchen kitchen = (Kitchen) o;
-        return id.equals(kitchen.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
--- src/main/java/br/com/alopes/deliveryfood/domain/model/Restaurant.java
+++ src/main/java/br/com/alopes/deliveryfood/domain/model/Restaurant.java
@@ -1,12 +1,16 @@
 package br.com.alopes.deliveryfood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) //gera somente para os campos que especifica ser incluso
 @Entity
 public class Restaurant {
 
@@ -17,41 +21,4 @@ public class Restaurant {
     private String name;
 
     private BigDecimal freightRate; // taxa de frete
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getFreightRate() {
-        return freightRate;
-    }
-
-    public void setFreightRate(BigDecimal freightRate) {
-        this.freightRate = freightRate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurant kitchen = (Restaurant) o;
-        return id.equals(kitchen.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
